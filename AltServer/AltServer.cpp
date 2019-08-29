59635b59957c7cf677a7eaa49dfa9476f947c71e
Riley Testut <riley@rileytestut.com>
2019-08-29 10:44:35 -0700

    Basic (and hacky) UI implementation

---
+++ AltServer/AltServer.cpp
@@ -0,0 +1,338 @@
+// HelloWindowsDesktop.cpp
+// compile with: /D_UNICODE /DUNICODE /DWIN32 /D_WINDOWS /c
+
+#include <windows.h>
+#include <windowsx.h>
+#include <stdlib.h>
+#include <string.h>
+#include <tchar.h>
+#include <CommCtrl.h>
+
+#include <strsafe.h>
+
+#include "resource.h"
+
+#pragma comment( lib, "gdiplus.lib" ) 
+#include <gdiplus.h> 
+
+#pragma comment(linker,"\"/manifestdependency:type='win32' \
+name='Microsoft.Windows.Common-Controls' version='6.0.0.0' \
+processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+
+// Global variables
+
+// The main window class name.
+static TCHAR szWindowClass[] = _T("DesktopApp");
+
+// The string that appears in the application's title bar.
+static TCHAR szTitle[] = _T("Windows Desktop Guided Tour Application");
+
+HINSTANCE hInst;
+
+// Forward declarations of functions included in this code module:
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+BOOL CALLBACK LoginDlgProc(HWND hwnd, UINT Message, WPARAM wParam, LPARAM lParam);
+
+int CALLBACK WinMain(
+	_In_ HINSTANCE hInstance,
+	_In_ HINSTANCE hPrevInstance,
+	_In_ LPSTR     lpCmdLine,
+	_In_ int       nCmdShow
+)
+{
+	WNDCLASSEX wcex;
+
+	wcex.cbSize = sizeof(WNDCLASSEX);
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc = WndProc;
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+	wcex.hInstance = hInstance;
+	wcex.hIcon = LoadIcon(hInstance, IDI_APPLICATION);
+	wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wcex.lpszMenuName = NULL;
+	wcex.lpszClassName = szWindowClass;
+	wcex.hIconSm = LoadIcon(wcex.hInstance, IDI_APPLICATION);
+
+	if (!RegisterClassEx(&wcex))
+	{
+		MessageBox(NULL,
+			_T("Call to RegisterClassEx failed!"),
+			_T("Windows Desktop Guided Tour"),
+			NULL);
+
+		return 1;
+	}
+
+	// Store instance handle in our global variable
+	hInst = hInstance;
+
+	// The parameters to CreateWindow explained:
+	// szWindowClass: the name of the application
+	// szTitle: the text that appears in the title bar
+	// WS_OVERLAPPEDWINDOW: the type of window to create
+	// CW_USEDEFAULT, CW_USEDEFAULT: initial position (x, y)
+	// 500, 100: initial size (width, length)
+	// NULL: the parent of this window
+	// NULL: this application does not have a menu bar
+	// hInstance: the first parameter from WinMain
+	// NULL: not used in this application
+	/*HWND hWnd = CreateWindow(
+		szWindowClass,
+		szTitle,
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT,
+		500, 100,
+		NULL,
+		NULL,
+		hInstance,
+		NULL
+	);*/
+
+	HWND hWnd = CreateWindowEx(
+		WS_EX_CLIENTEDGE,
+		szWindowClass,
+		szTitle,
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		500,
+		300,
+		HWND_MESSAGE,
+		NULL,
+		hInstance,
+		NULL
+	);
+
+	if (!hWnd)
+	{
+		MessageBox(NULL,
+			_T("Call to CreateWindow failed!"),
+			_T("Windows Desktop Guided Tour"),
+			NULL);
+
+		return 1;
+	}
+
+	// The parameters to ShowWindow explained:
+	// hWnd: the value returned from CreateWindow
+	// nCmdShow: the fourth parameter from WinMain
+	ShowWindow(hWnd, nCmdShow);
+	UpdateWindow(hWnd);
+
+	Gdiplus::GdiplusStartupInput gdiplusStartupInput;
+	ULONG_PTR gdiplusToken;
+	Gdiplus::GdiplusStartup(&gdiplusToken, &gdiplusStartupInput, NULL);
+
+	static const wchar_t* filename = L"MenuBarIcon.png";
+
+	Gdiplus::Bitmap* image = Gdiplus::Bitmap::FromFile(filename);
+	HICON hicon;
+	image->GetHICON(&hicon);
+
+	NOTIFYICONDATA niData;
+	ZeroMemory(&niData, sizeof(NOTIFYICONDATA));
+	niData.uVersion = NOTIFYICON_VERSION_4;
+	niData.cbSize = sizeof(NOTIFYICONDATA);
+	niData.uID = 10456;
+	niData.uFlags = NIF_ICON | NIF_MESSAGE | NIF_INFO | NIF_TIP | NIF_GUID;
+	niData.hWnd = hWnd;
+	niData.hIcon = hicon;
+	niData.uCallbackMessage = WM_USER + 1;
+	niData.uTimeout = 3000;
+	niData.dwInfoFlags = NIIF_INFO;
+	StringCchCopy(niData.szInfoTitle, ARRAYSIZE(niData.szInfoTitle), L"AltServer running.");
+	StringCchCopy(niData.szInfo, ARRAYSIZE(niData.szInfo), L"AltServer will continue to run in the background.");
+
+	//TODO: Load correct variant
+	HICON icon = (HICON)LoadImage(hInst, MAKEINTRESOURCE(IMG_MENUICON), IMAGE_ICON, GetSystemMetrics(SM_CXSMICON), GetSystemMetrics(SM_CYSMICON), LR_DEFAULTCOLOR);
+
+	Shell_NotifyIcon(NIM_ADD, &niData);
+
+	// Main message loop:
+	MSG msg;
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	return (int)msg.wParam;
+}
+
+#define ID_MENU_CLOSE 103
+
+//  FUNCTION: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  PURPOSE:  Processes messages for the main window.
+//
+//  WM_PAINT    - Paint the main window
+//  WM_DESTROY  - post a quit message and return
+
+static HMENU hPopupMenu = NULL;
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	PAINTSTRUCT ps;
+	HDC hdc;
+	TCHAR greeting[] = _T("Click to Log In.");
+
+	switch (message)
+	{
+	case (WM_USER + 1):
+	{
+		switch (lParam)
+		{
+		case WM_LBUTTONUP:
+		{
+			HMENU installMenu = CreatePopupMenu();
+			//AppendMenu(installMenu, MF_STRING | MF_GRAYED, 201, L"No Connected Devices");
+			AppendMenu(installMenu, MF_STRING, 202, L"Riley's iPhone X");
+
+			hPopupMenu = CreatePopupMenu();
+			AppendMenu(hPopupMenu, MF_STRING, 101, L"About AltServer");
+			AppendMenu(hPopupMenu, MF_STRING | MF_POPUP, (UINT)installMenu, L"Install AltStore");
+			AppendMenu(hPopupMenu, MF_STRING, ID_MENU_CLOSE, L"Close");
+
+			// Get the position of the cursor
+			POINT pCursor;
+
+			GetCursorPos(&pCursor);
+			// Popup the menu with cursor position as the coordinates to pop it up
+
+			SetForegroundWindow(hWnd);
+
+			int id = TrackPopupMenu(hPopupMenu, TPM_LEFTBUTTON | TPM_RIGHTALIGN | TPM_RETURNCMD, pCursor.x, pCursor.y, 0, hWnd, NULL);
+
+			PostMessage(hWnd, WM_NULL, 0, 0);
+
+			switch (id)
+			{
+			case ID_MENU_CLOSE:
+				PostMessage(hWnd, WM_CLOSE, 0, 0);
+				break;
+
+			case 202:
+			{
+				int result = DialogBox(NULL, MAKEINTRESOURCE(ID_LOGIN), hWnd, LoginDlgProc);
+				OutputDebugStringA("Finished!");
+
+				break;
+			}
+			}
+		}
+		default: break;
+		}
+
+		break;
+	}
+	case WM_PAINT:
+		hdc = BeginPaint(hWnd, &ps);
+
+		// Here your application is laid out.
+		// For this introduction, we just print out "Hello, Windows desktop!"
+		// in the top left corner.
+		TextOut(hdc,
+			5, 5,
+			greeting, _tcslen(greeting));
+		// End application-specific layout section.
+
+		EndPaint(hWnd, &ps);
+		break;
+
+	case WM_LBUTTONDOWN:
+	{
+		int result = DialogBox(NULL, MAKEINTRESOURCE(ID_LOGIN), hWnd, LoginDlgProc);
+		OutputDebugStringA("Finished!");
+
+		break;
+	}
+
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+		break;
+	}
+
+	return 0;
+}
+
+BOOL CALLBACK LoginDlgProc(HWND hwnd, UINT Message, WPARAM wParam, LPARAM lParam)
+{
+	HWND appleIDTextField = GetDlgItem(hwnd, IDC_EDIT1);
+	HWND passwordTextField = GetDlgItem(hwnd, IDC_EDIT2);
+	HWND installButton = GetDlgItem(hwnd, IDOK);
+
+	switch (Message)
+	{
+	case WM_INITDIALOG:
+	{
+		Edit_SetCueBannerText(appleIDTextField, _T("Apple ID"));
+		Edit_SetCueBannerText(passwordTextField, _T("Password"));
+
+		Button_Enable(installButton, false);
+
+		break;
+	}
+
+	case WM_CTLCOLORSTATIC:
+	{
+		if (GetDlgCtrlID((HWND)lParam) == IDC_DESCRIPTION)
+		{
+			HBRUSH success = (HBRUSH)GetStockObject(HOLLOW_BRUSH);
+			SetBkMode((HDC)wParam, TRANSPARENT);
+			return (BOOL)success;
+		}
+
+		break;
+	}
+
+	case WM_COMMAND:
+		switch (HIWORD(wParam))
+		{
+		case EN_CHANGE:
+		{
+			/*PostMessage(hWnd, WM_CLOSE, 0, 0);
+			break;*/
+
+			int appleIDLength = Edit_GetTextLength(appleIDTextField);
+			int passwordLength = Edit_GetTextLength(passwordTextField);
+
+			if (appleIDLength == 0 || passwordLength == 0)
+			{
+				Button_Enable(installButton, false);
+			}
+			else
+			{
+				Button_Enable(installButton, true);
+			}
+
+			break;
+		}
+		}
+
+		switch (LOWORD(wParam))
+		{
+		case IDOK:
+			wchar_t appleID[512];
+			wchar_t password[512];
+
+			Edit_GetText(appleIDTextField, appleID, 512);
+			Edit_GetText(passwordTextField, password, 512);
+
+			EndDialog(hwnd, IDOK);
+			break;
+
+		case IDCANCEL:
+			EndDialog(hwnd, IDCANCEL);
+			break;
+		}
+
+		default:
+			return FALSE;
+	}
+	return TRUE;
+}
---
+++ AltServer/resource.h
@@ -0,0 +1,21 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Resource.rc
+//
+#define ID_LOGIN                        9
+#define IDD_FORMVIEW                    101
+#define IMG_MENUICON                    106
+#define IDC_EDIT1                       1001
+#define IDC_EDIT2                       1002
+#define IDC_DESCRIPTION                 1008
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        107
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1009
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
