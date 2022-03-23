e9ec7a0c9e8484487f0753a4e65d5fe90c9a7c50
JJTech0130 <53275876+JJTech0130@users.noreply.github.com>
2022-03-23 17:32:23 -0400

    remove unused xcode folder

--- Xcode/AltServer-Windows/AltServer-Windows/Windows/ServerError.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  ServerError.cpp
-//  AltServer-Windows
-//
-//  Created by Riley Testut on 8/13/19.
-//  Copyright © 2019 Riley Testut. All rights reserved.
-//
-
-#include "ServerError.hpp"
